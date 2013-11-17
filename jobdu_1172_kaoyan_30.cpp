8b603b5f75d969bb105d073e1ffb52682e43eff6
fomalhautzhu <pro_standout@yahoo.com.cn>
2013-11-17 17:39:30 +0800

    1172

---
+++ jobdu_1172_kaoyan_30.cpp
@@ -0,0 +1,29 @@
+#include <cstdio>
+#include <queue>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+    int n, i, m, a, b, sum;
+    priority_queue<int, vector<int>, greater<int> > Q;
+    while (scanf("%d", &n) != EOF) {
+        while (!Q.empty())
+            Q.pop();
+        for (i = 0; i < n; i++) {
+            scanf("%d", &m);
+            Q.push(m);
+        }
+        sum = 0;
+        while (Q.size() > 1) {
+            a = Q.top();
+            Q.pop();
+            b = Q.top();
+            Q.pop();
+            sum += (a+b);
+            Q.push(a+b);
+        }
+        printf("%d\n", sum);       
+    }
+    return 0;
+}
